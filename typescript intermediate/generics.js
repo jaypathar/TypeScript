505bd9b644fb4d1ea7e11332cea89a3c75e23071
jaypathar <jayp.albiorix@gmail.com>
2023-07-25 10:33:49 +0530

    generics/modules

---
+++ typescript intermediate/generics.js
@@ -0,0 +1,12 @@
+/**
+ * Generics allow creating 'type variables' which can be used to create classes, functions & type aliases
+ * that don't need to explicitly define the types that they use.
+ * Generics is a way of  making code reusable.
+ */
+// the function is mainly returning data.however the data passed as parameter and returned can be any type
+function getValue(data) {
+    return data;
+}
+console.log(getValue(23)); // will return a integer value.
+console.log(getValue("John")); // will return a string value.
+console.log(getValue({ name: "John", age: '23' }));
